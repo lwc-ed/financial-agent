e11e5e4b67dbccb65b96866e5738795496bdcb45
KUAN-HSUN, CHU <hsun.mg12@nycu.edu.tw>
2025-12-30 23:13:00 +0800

    更改台灣時間

--- backend/models/wishlist.py
+++ backend/models/wishlist.py
@@ -1,13 +1,20 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DECIMAL, TIMESTAMP
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime # 改用 DateTime 比較單純
 from sqlalchemy.sql import func
 from backend.database import Base
 from backend.models.user import User
+from datetime import datetime
+import pytz
+
+def get_taiwan_time():
+    tw = pytz.timezone("Asia/Taipei")
+    return datetime.now(tw)
 
 class Wishlist(Base):
     __tablename__ = "wishlist"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    no = Column(Integer, primary_key=True, autoincrement=True) 
+    
     user_id = Column(Integer, ForeignKey(User.id))
     item_name = Column(String(255))
     price = Column(Integer)
     achieved = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), default=get_taiwan_time)
